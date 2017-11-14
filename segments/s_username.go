494a07724839a8204b78165039a708072ff06761
Romain Giraud <dev@romaingiraud.com>
2017-11-14 21:57:02 +0100

    Rework old segments

--- segments/s_username.go
+++ segments/s_username.go
@@ -1,14 +1,34 @@
 package segments
 
 import(
+    "fmt"
+    "log"
     "os/user"
+    "encoding/json"
 )
 
 
 type Username struct {
+    style Style
 }
 
-func (u Username) String() string {
+func (u Username) Print(context Context, index int) {
     uc, _ := user.Current()
-    return uc.Username
+    fmt.Print(u.style.Format(uc.Username, context, index))
+}
+
+func (u Username) GetStyle() Style {
+    return u.style
+}
+
+type usernameConfig struct {
+}
+
+func NewUsername(bytes json.RawMessage, style Style) Segment {
+    var config usernameConfig
+    err := json.Unmarshal(bytes, &config)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return &Username{ style }
 }
