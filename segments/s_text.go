da8b3445f65e75139af008fd287c0ee6dfc7a0c7
Romain Giraud <me@romaingiraud.com>
2017-12-12 16:49:33 +0100

    segments: interpret ${cmd> test} in text segment

--- segments/s_text.go
+++ segments/s_text.go
@@ -2,6 +2,10 @@ package segments
 
 import(
     "os"
+    "os/exec"
+    "strings"
+    "bytes"
+    "log"
 )
 
 
@@ -10,8 +14,30 @@ type Text struct {
     value string
 }
 
+func getenv(key string) string {
+    // Escape $ by doubling it: $$
+    if key == "$" {
+        return "$"
+    }
+
+    // Execute "my_command" from ${cmd> my_command}
+    if strings.HasPrefix(key, "cmd> ") {
+        cmd := exec.Command("bash", "-c", key[5:])
+        var out bytes.Buffer
+        cmd.Stdout = &out
+        err := cmd.Run()
+        if err != nil {
+            log.Fatal(err)
+        }
+        return strings.Trim(out.String(), "\n")
+    }
+
+    return os.Getenv(key)
+}
+
 func (t Text) Print(segments []Segment, current int) {
-    FormatString(os.ExpandEnv(t.value), t.style, segments, current)
+    str := os.Expand(t.value, getenv)
+    FormatString(str, t.style, segments, current)
 }
 
 func (t Text) GetStyle(segments []Segment, current int) Style {
