a4e10fa97f288a911ac66fd0afe1c1f5f1032627
Romain Giraud <dev@romaingiraud.com>
2017-12-13 19:47:03 +0100

    segments: ComplexPath: add maxDepth and ellipsis symbol

--- segments/s_complexpath.go
+++ segments/s_complexpath.go
@@ -12,6 +12,8 @@ type ComplexPath struct {
     style Style
     separator string
     fgSeparator Brush
+    maxDepth uint
+    ellipsis string
 }
 
 func (p ComplexPath) Print(writer io.Writer, segments []Segment, current int) {
@@ -19,11 +21,20 @@ func (p ComplexPath) Print(writer io.Writer, segments []Segment, current int) {
     if err != nil {
         log.Fatal(err)
     }
+
     home_dir := os.Getenv("HOME")
     if strings.Index(dir, home_dir) == 0 {
         dir = strings.Replace(dir, home_dir, "~", 1)
     }
+
     dir_s := strings.Split(dir, "/")
+    if len(dir_s) > int(p.maxDepth) {
+        dir_s = dir_s[len(dir_s) - int(p.maxDepth):]
+        dir_s[0] = p.ellipsis
+    }
+    for i, v := range dir_s {
+        dir_s[i] = " " + v + " "
+    }
 
     FormatStringArray(writer, dir_s, p.style, p.separator, p.fgSeparator, segments, current)
 }
@@ -33,5 +44,5 @@ func (p ComplexPath) GetStyle(segments []Segment, current int) Style {
 }
 
 func NewComplexPath(style Style, separator string, fgSeparator Brush) Segment {
-    return &ComplexPath{ style, separator, fgSeparator }
+    return &ComplexPath{ style, separator, fgSeparator, 5, "..." }
 }
