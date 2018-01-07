482337d9fb8876e15d6a76e98c79edd7d0f9f90f
Romain Giraud <me@romaingiraud.com>
2018-01-07 18:33:08 +0100

    Rename package segments to segment (in singular)
    Issue: #21
    
    Following style guidelines

--- segment/s_hostname.go
+++ segment/s_hostname.go
@@ -1,4 +1,4 @@
-package segments
+package segment
 
 import (
 	"github.com/RomainGiraud/gompt/format"
--- segments/doc.go
+++
@@ -1,3 +0,0 @@
-// Package segments regroups segment interface and sample segments.
-// Each prompt element must implement Segment interface.
-package segments
